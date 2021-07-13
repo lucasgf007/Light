514e1a00782d0825fba5bc994e173c6235d3ff05
Lucas Gabriel <lucas.gabriel.017@ufrn.edu.br>
2021-07-13 18:01:01 -0300

    meu primeiro commit 2

---
+++ script.js
@@ -0,0 +1,11 @@
+function mudar(){
+
+    for(let i = 0; i < 10; i++){
+        document.getElementById('luz').src = 'img/Lon.png';
+	} 
+	                           
+}
+
+function apagar(){
+    document.getElementById('luz').src = 'img/Loff.png';
+}
